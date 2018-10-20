e14ac9df6fb52958a2d3725f950a9eef8c861122
Kevine Musoni <kevinenicky@gmail.com>
2018-10-20 21:00:11 +0300

    sidebar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WritersComponent } from './writers/writers.component';
+import { ProfileComponent } from './profile/profile.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    // this path will show all the writers
+    path: '',
+    component: WritersComponent
+  },
+  {
+    // this path will show all the writer's profile
+    path: 'profile/:id',
+    component: ProfileComponent
+  },
+  {
+    path: 'quotes',
+    component: QuotesComponent
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { WritersComponent } from './writers/writers.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    QuotesComponent,
+    WritersComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
