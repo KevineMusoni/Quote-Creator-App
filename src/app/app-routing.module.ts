f31b1209a4fc6c2d748cc555b3f03dc3206b49e3
Kevine Musoni <kevinenicky@gmail.com>
2018-10-22 01:00:53 +0300

    routing applyied

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { QuotesComponent } from './quotes/quotes.component';
-
+import { SidebarComponent } from './sidebar/sidebar.component';
 const routes: Routes = [
   {
     path: 'quotes',
     component: QuotesComponent
   },
+  {
+    path: 'sidebar',
+    component: SidebarComponent
+  }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [QuotesComponent, SidebarComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { ProfileFormComponent } from './profile-form/profile-form.component';
-import { RouterModule, Routes } from '@angular/router';
-
-const appRoutes: Routes = [
-  {path: '', component: ProfileFormComponent}
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    QuotesComponent,
-    ProfileFormComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
