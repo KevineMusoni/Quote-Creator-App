188e71440733f8af1c1d2bced5d364dcaf34ae15
Kevine Musoni <kevinenicky@gmail.com>
2018-10-21 22:24:59 +0300

    form validation

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  // log(x) { console.log(x); }
   constructor() { }
 
   ngOnInit() {
